655f3862441804f90e6c50a5de22fb76236fb754
omadushika <madushikaoshan2018@gmail.com>
2022-10-04 10:11:33 +0530

    OCT 02/ 3rd week/ Js

---
+++ Js/function.js
@@ -0,0 +1,16 @@
+
+
+
+function printFullName() {
+    var firstname = "Oshan";
+var lastname = "Madushika";
+var isCheck = true;
+if (isCheck) {
+    console.log(firstname + " " + lastname);
+}
+else {
+    console.log("error");
+}
+}
+
+printFullName();
---
+++ Js/index.js
@@ -0,0 +1,9 @@
+var Name = "Oshan Madushika";
+var Surname = "H.T.A.";
+var isCheck = !true; 
+if (isCheck) {
+    console.log(Surname + " " + Name);
+}
+else {
+    console.log("Error")
+}
