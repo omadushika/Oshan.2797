edb4d21fa5fedcb45b82eff8b64510faa65b8fd5
omadushika <madushikaoshan2018@gmail.com>
2022-10-09 10:25:13 +0530

    no message

--- Js/myPractice.js
+++ Js/myPractice.js
@@ -45,3 +45,13 @@ car = [];
 car.push('Oshan');
 console.log(car);
 */
+
+var readline = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout 
+});
+
+readline.question("what is your name?", function(name){
+    console.log("hello " +name);
+    readline.close();
+})
--- Js/userinput.js
+++ Js/userinput.js
@@ -3,10 +3,12 @@ var readline = require('readline').createInterface({
     output: process.stdout 
 });
 
+/*
 readline.question("what is your name?", function(name){
     console.log("hello " +name);
     readline.close();
 })
+*/
 
 readline.question("what is your age?", function(age){
     console.log("welcome, your age entered is " +age);
